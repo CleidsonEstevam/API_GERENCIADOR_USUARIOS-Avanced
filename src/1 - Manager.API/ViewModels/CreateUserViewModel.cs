d65075a79f26b01e9d9c1200d56d8e3d0c8ded81
Cleidson <cleidsonestevamdasilva@hotmail.com>
2022-01-14 23:26:29 -0300

    Alterações na ViewModel de User

--- src/1 - Manager.API/ViewModels/CreateUserViewModel.cs
+++ src/1 - Manager.API/ViewModels/CreateUserViewModel.cs
@@ -7,18 +7,19 @@ namespace Manager.API.ViewModels
         [Required(ErrorMessage = "O Nome não pode ser nulo.")]
         [MinLength(3, ErrorMessage = "O Nome deve ter no mínimo 3 caracteres.")]
         [MaxLength(80, ErrorMessage = "O Nome deve ter no máximo 80 caracteres.")]
-        public string Name    { get; private set; }
+        public string Name    { get;  set; }
 
         [Required(ErrorMessage = "O Email não pode ser nulo.")]
         [MinLength(10, ErrorMessage = "O Email deve ter no mínimo 3 caracteres.")]
         [MaxLength(180, ErrorMessage = "O Email deve ter no máximo 80 caracteres.")]
-        //Falta add regular expression do email
-        public string Email    { get; private set; }
+        [RegularExpression(@"^([\w-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([\w-]+\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$",
+                        ErrorMessage = "O email informado não é válido.")]
+        public string Email    { get;  set; }
 
         [Required(ErrorMessage = "O Password não pode ser nulo.")]
         [MinLength(3, ErrorMessage = "O Password deve ter no mínimo 3 caracteres.")]
         [MaxLength(80, ErrorMessage = "O Password deve ter no máximo 80 caracteres.")]
-        public string Password { get; private set; }
+        public string Password { get;  set; }
 
     }
 }
