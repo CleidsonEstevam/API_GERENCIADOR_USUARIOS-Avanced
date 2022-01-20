27b7b41f58036c33844006b2eae3cb8a8832415c
Cleidson <cleidsonestevamdasilva@hotmail.com>
2022-01-20 20:55:34 -0300

    Camada de Testes estruturada

--- src/6 - Manager.Tests/Projects/Services/UserServiceTests.cs
+++ src/6 - Manager.Tests/Projects/Services/UserServiceTests.cs
@@ -5,6 +5,15 @@ using Manager.Infra.Interfaces;
 using EscNet.Cryptography.Interfaces;
 using Manager.Services.Services;
 using Manager.Tests.Configurations;
+using Xunit;
+using Manager.Services.DTO;
+using Manager.Domain.Entities;
+using Bogus.DataSets;
+using System.Linq.Expressions;
+using System;
+using FluentAssertions;
+using System.Threading.Tasks;
+using Manager.Tests.Fixtures;
 
 namespace Manager.Tests.Projects.Services
 {
@@ -22,15 +31,54 @@ namespace Manager.Tests.Projects.Services
 
      public UserServiceTests()
      {
-        _mapper = AutoMapperConfiguration.GetConfiguration();
+        _mapper = AutoMapperConfiguration.GetConfiguration() ;
         _userRepositoryMock = new Mock<IUserRepository>();
         _rijndaelCryptographyMock = new Mock<IRijndaelCryptography>();
 
           _sut = new UserServices(
                 mapper: _mapper,
                 userRepository: _userRepositoryMock.Object,
-                rijndaelCryptography: _rijndaelCryptographyMock.Object
-        );
-     }
+                rijndaelCryptography: _rijndaelCryptographyMock.Object);
+     }  
+     
+        //METODO_CONDICAO_RESULTADOESPERADO
+      [Fact(DisplayName = "Create Valid User")]
+     public async Task Create_WhenUserIsValid_ReturnsUserDTO()
+      {
+          // ARRANGE -- REUNE O TESTE 
+           // var userToCreate = new UserDTO{Name = "Cleidson", Email = "teste@gmail.com", Password = "123321321"}; //Criando com instancia
+           var userToCreate = UserFixture.CreateValidUserDTO();
+           
+
+           var encryptedPassword = new Lorem().Sentence();
+            var userCreated = _mapper.Map<User>(userToCreate);
+             userCreated.ChangePassword(encryptedPassword);
+
+           _userRepositoryMock.Setup(x => x.GetAsync(
+                 It.IsAny<long>()))
+            .ReturnsAsync(() => null);
+
+            _rijndaelCryptographyMock.Setup(x => x.Encrypt(It.IsAny<string>()))
+                .Returns(encryptedPassword);
+
+            _userRepositoryMock.Setup(x => x.CreateAsync(It.IsAny<User>()))
+                .ReturnsAsync(() => userCreated);
+              
+          // ACT -- REALIZA AS CHAMADAS 
+            var result = await _sut.Crate(userToCreate);
+
+          //ASSERT  -- VERIFICA O RESULTADO
+         //Assert.Equal(result, userToCreate); --  Metodo de retorno padrão
+         //Metodo da bilioteca fluentassertions
+         result.Should()
+                    .BeEquivalentTo(_mapper.Map<UserDTO>(userCreated));
+      }  
+
+        
+
+
+
+
     }
+    
 }
